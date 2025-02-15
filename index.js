519dc161c5a4e6e5703f7f005fef30bf56640ee1
mattbarcelona <mathieu.driencourt@yahoo.fr>
2025-02-15 16:27:13 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// index.js
+
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const { dbConnection } = require("./config/config");
+const routes = require("./routes");
+
+app.use(express.json());
+
+app.use("/", routes);
+
+dbConnection();
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
